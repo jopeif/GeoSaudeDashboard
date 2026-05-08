084309f84aab5b3cddf1cf5bb6911c1cc51d5367
jopeif <joao.pedro.lima06@aluno.ifce.edu.br>
2026-05-08 13:07:24 -0300

    feat: update Heatmap map attribution and URL; enhance KPI card styling

--- src/pages/Heatmap/HeatmapPage.tsx
+++ src/pages/Heatmap/HeatmapPage.tsx
@@ -162,8 +162,8 @@ export const HeatmapPage = () => {
           className="leaflet-main-map"
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           />
           <HeatmapLayer points={points} />
         </MapContainer>
